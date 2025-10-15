1b5437dacee39fb65d6f8fde41fe9f5064858ad3
QHanh <hqh147@gmail.com>
2025-10-15 10:22:45 +0700

    up

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from src.main import app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8018, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8018, reload=False)
